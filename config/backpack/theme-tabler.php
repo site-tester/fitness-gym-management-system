6b16af063aa9218efd2efafc8a64b9580b30c78a
mccescario1995 <mccescario@gmail.com>
2025-02-04 19:31:07 +0800

    update config

--- config/backpack/theme-tabler.php
+++ config/backpack/theme-tabler.php
@@ -62,7 +62,7 @@ return [
         /**
          * The color mode used by default.
          */
-        'defaultColorMode' => 'dark', // system, light, dark
+        'defaultColorMode' => 'light', // system, light, dark
 
         /**
          * When true, a switch is displayed to let admins choose their favorite theme mode.
