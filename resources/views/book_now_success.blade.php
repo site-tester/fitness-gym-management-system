722bdb72e054bb8aeda80bda1110d396a4e6bb1d
mccescario1995 <mccescario@gmail.com>
2025-02-09 17:01:08 +0800

    Update background image and enhance booking success message layout

--- resources/views/book_now_success.blade.php
+++ resources/views/book_now_success.blade.php
@@ -30,7 +30,7 @@
         /* /* FORMS */
 
         #qbox-container {
-            background: url(../img/corona.png);
+            background: url(../storage/img/Logo.jpg);
             background-repeat: repeat;
             position: relative;
             /* padding: 62px; */
@@ -280,7 +280,8 @@
     <div class="container-fluid">
         <div class="row shadow mt-5 mx-auto w-50 p-0 border border-danger">
             <div class="col-7 px-0 overflow-hidden">
-                <img src="{{ asset('img/Logo.jpg') }}" alt="" style="width: 100%; height: 100%; object-fit: cover;">
+                <img src="{{ asset('/resources/img/Logo.jpg') }}" alt=""
+                    style="width: 100%; height: 100%; object-fit: cover;">
             </div>
             <div class="col-5 px-0 border-start border-danger border-2">
                 <div class="rounded-0 ">
@@ -292,12 +293,21 @@
                     <div id="qbox-container">
                         <div id="steps-container">
                             <div>
-                                <div class="mt-5 p-5">
-                                    <h4 class="fw-bold text-danger">Congratulations!</h4>
-                                    <h5>Booking Successful</h5>
-                                    <p>Please adhere to the gym's rules and regulations during your visit. For
+                                <div class="mt-5 p-1 p-md-5">
+                                    <div class="text-center px-3 mb-4">
+
+                                        <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1"
+                                            viewBox="0 0 24 24" style="transform: scale(.7);">
+                                            <path
+                                                d="M11.798,10.814l.279,.188c.466-.334,5.858-3.846,6-7.478,0-1.944-1.458-3.525-3.25-3.525-1.156,0-2.174,.659-2.75,1.648-.576-.989-1.594-1.648-2.75-1.648-1.792,0-3.25,1.581-3.25,3.525,0,3.395,5.487,7.132,5.721,7.29ZM9.327,1c1.241,0,2.25,1.133,2.25,2.525h1c0-1.392,1.01-2.525,2.25-2.525s2.25,1.133,2.25,2.525c0,2.239-3.38,5.111-5,6.266-1.621-1.155-5-4.027-5-6.266,0-1.392,1.009-2.525,2.25-2.525Zm14.673,16h-1v-4h-3v-2h-4v6H8v-6H4v2H1v4H0v1H1v4h3v2h4v-6h8v6h4v-2h3v-4h1v-1ZM2,21v-7h2v7H2Zm5,2h-2V12h2v11Zm12,0h-2V12h2v11Zm3-2h-2v-7h2v7Z" />
+                                        </svg>
+
+                                    </div>
+                                    <h4 class="fw-bold text-danger text-center" style="font-size: 1.5vw;">Congratulations!</h4>
+                                    <h5 class="text-center">Booking Successful</h5>
+                                    <p class="text-center">Please adhere to the gym's rules and regulations during your visit. For
                                         cancellations or inquiries, feel free to contact us. Enjoy your workout!</p>
-                                    <a class="back-link" href="/book-now">Go back from the beginning ➜</a>
+                                    <a class="back-link text-center" href="/book-now">Go back from the beginning ➜</a>
                                 </div>
                             </div>
                         </div>
