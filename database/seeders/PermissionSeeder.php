02cf3087a476cb48a260dafdd637ae7bb6ac5529
mccescario1995 <mccescario@gmail.com>
2025-02-11 22:51:51 +0800

    Implement user permission checks in menu items and add PermissionSeeder for role management

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'manage-clients',
+            'manage-trainers',
+            'manage-bookings',
+            'manage-payments',
+            'manage-payment-methods',
+            'manage-gym',
+            'manage-gym-workouts',
+            'manage-gym-services',
+            'manage-gym-inventory',
+            'manage-gym-equipment',
+            'manage-contact-inbox',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission, 'guard_name' => 'web']);
+        }
+
+        $this->command->info('Permissions seeded successfully.');
+
+        $adminPermissions = Permission::all();
+        $this->command->info('Admin permissions: ' . implode(', ', $adminPermissions->pluck('name')->toArray()));
+
+        $trainerPermissions = [
+            'manage-clients',
+            'manage-bookings',
+            'manage-gym',
+            'manage-gym-workouts',
+            'manage-gym-services',
+            'manage-gym-inventory',
+            'manage-gym-equipment',
+        ];
+
+        $this->command->info('Trainer permissions: ' . implode(', ', $trainerPermissions));
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $adminRole->givePermissionTo($adminPermissions);
+
+        $trainerRole = Role::where('name', 'trainer')->first();
+        $trainerRole->givePermissionTo($trainerPermissions);
+    }
+}
--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -10,33 +10,56 @@
 {{-- <x-backpack::menu-item title='Notifications' icon='la la-exclamation-triangle' :link="backpack_url('')" /> --}}
 {{-- End Notification --}}
 
-<x-backpack::menu-dropdown title="Manage Users" icon="la la-user">
-    <x-backpack::menu-dropdown-header title="Client" />
-    <x-backpack::menu-dropdown-item title="Client Details" icon="la la-user" :link="backpack_url('membership-detail')" />
-    <x-backpack::menu-dropdown-item title="Client Progress" icon="la la-chart-bar" :link="backpack_url('gym-progress')" />
-    <x-backpack::menu-dropdown-item title="Add Client Details" icon="la la-user-plus" :link="backpack_url('membership-detail/create')" />
-    <x-backpack::menu-dropdown-header title="Trainer" />
-    <x-backpack::menu-dropdown-item title="Trainer Details" icon="la la-user" :link="backpack_url('trainer-detail')" />
-    <x-backpack::menu-dropdown-item title="Add Trainer Details" icon="la la-user-plus" :link="backpack_url('trainer-detail/create')" />
-</x-backpack::menu-dropdown>
+@can('manage-users')
+    <x-backpack::menu-dropdown title="Manage Users" icon="la la-user">
+        @can('manage-clients')
+            <x-backpack::menu-dropdown-header title="Client" />
+            <x-backpack::menu-dropdown-item title="Client Details" icon="la la-user" :link="backpack_url('membership-detail')" />
+            <x-backpack::menu-dropdown-item title="Client Progress" icon="la la-chart-bar" :link="backpack_url('gym-progress')" />
+            <x-backpack::menu-dropdown-item title="Add Client Details" icon="la la-user-plus" :link="backpack_url('membership-detail/create')" />
+        @endcan
+        @can('manage-trainers')
+            <x-backpack::menu-dropdown-header title="Trainer" />
+            <x-backpack::menu-dropdown-item title="Trainer Details" icon="la la-user" :link="backpack_url('trainer-detail')" />
+            <x-backpack::menu-dropdown-item title="Add Trainer Details" icon="la la-user-plus" :link="backpack_url('trainer-detail/create')" />
+        @endcan
+    </x-backpack::menu-dropdown>
+@endcan
 
-<x-backpack::menu-item title="Bookings" icon="la la-book" :link="backpack_url('reservation')" />
+@can('manage-bookings')
+    <x-backpack::menu-item title="Bookings" icon="la la-book" :link="backpack_url('reservation')" />
+@endcan
 
-<x-backpack::menu-dropdown title="Payments" icon="la la-wallet">
-    <x-backpack::menu-dropdown-item title='Payments' icon='la la-money-bill' :link="backpack_url('payment')" />
-    <x-backpack::menu-dropdown-item title='Payment Methods' icon='la la-credit-card' :link="backpack_url('payment-method')" />
-</x-backpack::menu-dropdown>
+@can('manage-payments')
+    <x-backpack::menu-dropdown title="Payments" icon="la la-wallet">
+        <x-backpack::menu-dropdown-item title='Payments' icon='la la-money-bill' :link="backpack_url('payment')" />
+        @can('manage-payment-methods')
+            <x-backpack::menu-dropdown-item title='Payment Methods' icon='la la-credit-card' :link="backpack_url('payment-method')" />
+        @endcan
+    </x-backpack::menu-dropdown>
+@endcan
 
-<x-backpack::menu-dropdown title="Manage GYM" icon="la la-dumbbell">
-    <x-backpack::menu-dropdown-header title="Workouts" />
-    <x-backpack::menu-dropdown-item title="Workouts" icon="la la-dumbbell" :link="backpack_url('workout')" />
-    <x-backpack::menu-dropdown-header title="Services" />
-    <x-backpack::menu-dropdown-item title="Service" icon="la la-hard-hat" :link="backpack_url('service')" />
-    <x-backpack::menu-dropdown-item title="Service Categories" icon="la la-clipboard-list" :link="backpack_url('service-category')" />
-    <x-backpack::menu-dropdown-item title="Service Amenities" icon="la la-puzzle-piece" :link="backpack_url('amenity')" />
-    <x-backpack::menu-dropdown-header title="Inventories" />
-    <x-backpack::menu-dropdown-item title='Inventory' icon='la la-boxes' :link="backpack_url('inventory')" />
-    <x-backpack::menu-dropdown-item title="Equipment" icon="la la-box" :link="backpack_url('equipment-inventory')" />
-</x-backpack::menu-dropdown>
+@can('manage-gym')
+    <x-backpack::menu-dropdown title="Manage GYM" icon="la la-dumbbell">
+        @can('manage-gym-workouts')
+            <x-backpack::menu-dropdown-header title="Workouts" />
+            <x-backpack::menu-dropdown-item title="Workouts" icon="la la-dumbbell" :link="backpack_url('workout')" />
+        @endcan
+        @can('manage-gym-services')
+            <x-backpack::menu-dropdown-header title="Services" />
+            <x-backpack::menu-dropdown-item title="Service" icon="la la-hard-hat" :link="backpack_url('service')" />
+            <x-backpack::menu-dropdown-item title="Service Categories" icon="la la-clipboard-list" :link="backpack_url('service-category')" />
+            <x-backpack::menu-dropdown-item title="Service Amenities" icon="la la-puzzle-piece" :link="backpack_url('amenity')" />
+        @endcan
+        @can('manage-gym-inventory')
+            <x-backpack::menu-dropdown-header title="Inventories" />
+            <x-backpack::menu-dropdown-item title='Inventory' icon='la la-boxes' :link="backpack_url('inventory')" />
+            <x-backpack::menu-dropdown-item title="Equipment" icon="la la-box" :link="backpack_url('equipment-inventory')" />
+        @endcan
 
-<x-backpack::menu-item title="Contact Us Inbox" icon="la la-inbox" :link="backpack_url('contactus-inbox')" />
+    </x-backpack::menu-dropdown>
+@endcan
+
+@can('manage-contact-inbox')
+    <x-backpack::menu-item title="Contact Us Inbox" icon="la la-inbox" :link="backpack_url('contactus-inbox')" />
+@endcan
