b69e96be6c6861e5a545a1120f2f9fed8b560d39
mccescario1995 <mccescario@gmail.com>
2025-02-23 19:49:01 +0800

    Add Summernote assets to Backpack UI scripts and styles

--- resources/views/vendor/backpack/ui/inc/styles.blade.php
+++ resources/views/vendor/backpack/ui/inc/styles.blade.php
@@ -1,5 +1,6 @@
 @basset('https://unpkg.com/animate.css@4.1.1/animate.compat.css')
 @basset('https://unpkg.com/noty@3.2.0-beta-deprecated/lib/noty.css')
+@basset("https://unpkg.com/summernote@0.8.20/dist/summernote-lite.min.css")
 
 @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css')
 @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-regular-400.woff2')
