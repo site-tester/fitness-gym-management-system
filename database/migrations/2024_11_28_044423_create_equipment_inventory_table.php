b80855b5d4a47fee4171ca07ee71d6a53827906c
mccescario1995 <mccescario@gmail.com>
2025-02-26 09:11:31 +0800

    Comment out image field in equipment inventory table migration

--- database/migrations/2024_11_28_044423_create_equipment_inventory_table.php
+++ database/migrations/2024_11_28_044423_create_equipment_inventory_table.php
@@ -30,7 +30,7 @@ return new class extends Migration {
             $table->string('supplier_name')->nullable();
             $table->enum('usage_frequency', ['high', 'medium', 'low'])->default('medium')->nullable();
             $table->text('remarks')->nullable();
-            $table->string('image')->nullable();
+            // $table->string('image')->nullable();
             $table->foreignId('added_by')->constrained('users')->onDelete('cascade')->nullable();
             $table->timestamps();
         });
