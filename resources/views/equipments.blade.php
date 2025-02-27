0014853b6dd8c4e0b558893ea998f9ebbf6510da
mccescario1995 <148983239+mccescario1995@users.noreply.github.com>
2025-02-27 22:40:15 +0800

    test

--- resources/views/equipments.blade.php
+++ resources/views/equipments.blade.php
@@ -80,9 +80,9 @@ $(document).ready(function () {
                 if (response.steps) {
                     let steps = JSON.parse(response.steps);
                     if (steps && steps.length > 0) {
-                        stepsHtml = "<ol style='text-align: justify;'>";
+                        stepsHtml = "<ol style='text-align: justify; display: list-item;'>";
                         steps.forEach(function (step) {
-                            stepsHtml += "<li>" + step + "</li>";
+                            stepsHtml += "<li style='display: list-item;'>" + step + "</li>";
                         });
                         stepsHtml += "</ol>";
                     } else {
